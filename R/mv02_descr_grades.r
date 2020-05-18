8ffa3c2aaf2afd9869cb6795862ba35f26ea3f6b
David Gyllenberg <david.gyllenberg@utu.fi>
2020-05-18 15:59:41 +0300

    Correlation

--- R/mv02_descr_grades.r
+++ R/mv02_descr_grades.r
@@ -290,7 +290,9 @@ pair_fn <-
   function(data, mapping, ...) {
     p <- ggplot(data = data, mapping = mapping) +
       geom_jitter(size = 0.5) + 
-      geom_smooth(method=loess, fill="red", color="red", ...) +
+      geom_smooth(method=loess, ...
+                  # , fill="red", color="red", ...
+                  ) +
       # geom_smooth(method=lm, fill="blue", color="blue", ...) +
       theme_minimal()
     p
@@ -298,9 +300,9 @@ pair_fn <-
 
 fbc %>% 
   select(fas_dg, vars_avera, vars_grade) %>% 
-  sample_n(10) %>% # Sample for testing
+  sample_n(1000) %>% # Sample for testing
   GGally::ggpairs(
     columns = 2:ncol(.), 
-    # mapping = ggplot2::aes(color = fas_dg),
+    mapping = ggplot2::aes(color = fas_dg),
     lower = list(continuous = pair_fn))
 
